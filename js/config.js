a43b3699f55f96fe7c6b0bf5dbfbfff11338596a
Jen <38684038+yeahokthen@users.noreply.github.com>
2025-06-05 00:40:46 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://n2bpzjxph4.execute-api.eu-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
